fc8e958a57dba8715b1ed8cec2814b07ee75a8ed
lNewED <NewHaZao.4@gmail.com>
2023-03-23 12:02:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Athiphong</title>
+
+    <link href="data/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container-fluid p-3 my-3">
+        <div class="height-100 d-flex justify-content-center align-items-center">
+              <div class="card p-3">
+           
+              <div class="d-flex justify-content-between align-items-center">
+                <img src="https://www.npru.ac.th/2019/img/logo.png"width="400" />  
+                <img src="/image/322573524_1291583478329739_8256863091867098178_n.jpg" width="100" />         
+              </div>
+              
+
+              
+              <div class="p-4 card-border mt-4">
+                      <div class="d-flex justify-content-between align-items-center">
+                          <span class="cardholder"> ชื่อ-สกุล อธิพงษ์ หินอ่อน </span>
+                          <span class="expires"> รหัสนักศึกษา 644259026 </span>
+                      </div>
+
+                      <div class="d-flex justify-content-between align-items-center">
+                          <span class="cardholder">สาขาวิชา วิศวกรรมซอฟต์แวร์ </span>
+                      </div>
+                      
+                      <div class="px-2 number mt-3 d-flex justify-content-between align-items-center">
+                  <span>4732</span>
+                   <span>5285</span>
+                    <span>1809</span>
+                     <span>7479</span>
+              </div>
+
+                  <div class="p-4 card-border mt-4">
+                      <div class="d-flex justify-content-between align-items-center">
+                          <span class="cardholder">0701</span>
+                          <span class="expires">Expires</span>
+                      </div>
+
+                      <div class="d-flex justify-content-between align-items-center">
+                          <span class="name">Athiphong Hinoon </span>
+                          <span class="date">12/26</span>
+                      </div>
+
+                      
+                  </div>
+                     
+                     
+                      </div>
+                  </div>
+              </div>
+              
+        </div>
+       </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.jsM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
